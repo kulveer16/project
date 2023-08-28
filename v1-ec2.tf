de7ee8d1c752a55771f3f49f49592059021052f9
Sardar Singh <sardar_singh@epam.com>
2023-08-28 21:05:51 +0530

    instances setup

--- v1-ec2.tf
+++ v1-ec2.tf
@@ -3,22 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "demoserver" {
- ami = "ami-051f7e7f6c2f40dc1" 
+ ami = "ami-053b0d53c279acc90" 
  instance_type = "t2.micro"
  key_name = "learning"
- security_groups = ["demosg"]
+ vpc_security_group_ids = [ aws_security_group.demosg.id ]
+ subnet_id = aws_subnet.dpw-public_subent_01.id
+ for_each = toset(["Jenkins-master", "build-slave","ansibleserver"])
+   tags = {
+     Name = "${each.key}"
+   }
 
 }
 
 resource "aws_security_group" "demosg" {
   name        = "demosg"
   description = "Allow ssh inbound traffic"
+  vpc_id = aws_vpc.dpw-vpc.id
 
 
   ingress {
     description      = "ssh from VPC"
     from_port        = 22
-    to_port          = 2
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
    
@@ -35,4 +41,54 @@ resource "aws_security_group" "demosg" {
   tags = {
     Name = "ssh-port"
   }
+}
+
+  resource "aws_vpc" "dpw-vpc" {
+          cidr_block = "10.1.0.0/16"
+          tags = {
+           Name = "dpw-vpc"
+        }
+      }
+
+resource "aws_subnet" "dpw-public_subent_01" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_01"
+    }
+} 
+
+resource "aws_subnet" "dpw-public_subent_02" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_01"
+    }
+} 
+
+resource "aws_internet_gateway" "dpw-igw" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    tags = {
+      Name = "dpw-igw"
+    }
+}
+
+resource "aws_route_table" "dpw-public-rt" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpw-igw.id
+    }
+    tags = {
+      Name = "dpw-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-1" {
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+    route_table_id = aws_route_table.dpw-public-rt.id
 }
